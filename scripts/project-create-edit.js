60600a4f5ea2a97338390eb8105014e68bb53a66
Mend1s <andremendesskt99@gmail.com>
2022-08-10 19:44:19 -0300

    finalizando validações do js nas telas

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -1,7 +1,43 @@
-window.onload = function(){
-    // Type: 'Create' | 'edit'
-    const screenType = 'create';
+// Pega os paramêtros da URL
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
 
+// Type: 'Create' | 'edit'
+const screenType = params.id ? 'edit' : 'create';
+
+function createOrEdit() {
+    // Inicia a massa de dados (payload)
+    let payload = {
+        title: document.querySelector("#title").value,
+        totalCost: document.querySelector("#totalCost").value,
+        description: document.querySelector("#description").value,
+        idClient: "1"
+    }
+
+    // Enviar para API
+    fetch(`https://62e9b89901787ec7121bb21c.mockapi.io/api/projects${screenType == 'edit' ? ('/' + params.id) : ''}`, {
+        method: screenType === 'edit' ? 'PUT' : 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        if(screenType === 'edit'){
+            alert('Editado com sucesso!');
+        } else {
+            alert('Cadastrado com sucesso!')
+        }
+    })
+    
+}
+
+window.onload = function(){
+    setScreenTypesTexts();
+}
+
+function setScreenTypesTexts(){
     // MODO CRIAR
     if (screenType == 'create'){
         document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
@@ -10,7 +46,7 @@ window.onload = function(){
 
     // MODO EDITAR
     if(screenType == 'edit'){
-        document.querySelector('#main-title').innerText = "Editar projeto";
-        document.querySelector('#action-button').innerText = "Salvar";
+       document.querySelector('#main-title').innerText = "Editar projeto";
+       document.querySelector('#action-button').innerText = "Salvar";
     }
 }

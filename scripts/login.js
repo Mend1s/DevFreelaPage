a45e210bf4ee22cdb6626521e08a28f934104009
Mend1s <andremendesskt99@gmail.com>
2022-08-09 21:48:47 -0300

    terminando validação js

--- scripts/login.js
+++ scripts/login.js
@@ -13,16 +13,31 @@ function checkIfAnyRoleIsChecked(){
 
 
 function cadastrar() {
-
+    // Checa se alguma role foi checada.
     if(checkIfAnyRoleIsChecked() === false){
-        alert('Marque alguma Role!')
+        alert('Marque alguma Role!');
+        return;
+    }
+    // Inicia a massa de dados (payload)
+    let payload = {
+        role: document.getElementsByName("role")[0].checked == true ? 'dev' : 'cliente',
+        fullName: document.querySelector("#fullName").value,
+        birthdate: document.querySelector("#birthdate").value,
+        email: document.querySelector("#email").value,
+        password: document.querySelector("#password").value
     }
 
-    let payload = {
-        fullName: document.querySelector('#fullName').value,
-        birthdate: document.querySelector('#birthdate').value,
-        email: document.querySelector('#email').value,
-        password: document.querySelector('#password').value
-    }
+    // Enviar para API
+    fetch("https://62e9b89901787ec7121bb21c.mockapi.io/api/users", {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        alert('Cadastrado com sucesso!');
+    })
     
 }
